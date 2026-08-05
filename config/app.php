5b837def37d20a7b85abce9351d3c3308461415b
noufalyoga0 <noufalyoga0@student.ub.ac.id>
2026-08-05 16:41:03 +0700

    fix: set default config APP_URL to https://e-layanan-aptika-jombang-production.up.railway.app to fix Railway redirect scheme

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://e-layanan-aptika-jombang-production.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
