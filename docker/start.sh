095b7cae04e54028e5b8ccc9890798461d71a62f
noufalyoga0 <noufalyoga0@student.ub.ac.id>
2026-08-06 19:24:35 +0700

    fix: clear view & route cache before recaching on startup

--- docker/start.sh
+++ docker/start.sh
@@ -10,6 +10,8 @@ if [ -z "$APP_KEY" ]; then
 fi
 
 php artisan config:clear
+php artisan view:clear
+php artisan route:clear
 php artisan migrate --force
 php artisan db:seed --force
 php artisan storage:link 2>/dev/null || true
