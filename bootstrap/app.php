2c031de914e2dcc9d6c21871587403fc1ae8aa10
noufalyoga0 <noufalyoga0@student.ub.ac.id>
2026-08-10 08:52:01 +0700

    revert: hapus total fitur forced password change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,9 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
 
         $middleware->alias([
-            'role'             => \App\Http\Middleware\CheckRole::class,
-            'guest'            => \App\Http\Middleware\RedirectIfAuthenticated::class,
-            'force.pw.change'  => \App\Http\Middleware\ForcedPasswordChange::class,
+            'role'  => \App\Http\Middleware\CheckRole::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
